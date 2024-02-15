a965925f51c1ae0acd892fc417b78d0e7e906604
wl4dig3 <wladige@gmail.com>
2024-02-15 00:04:28 -0300

    feat: boilerplate

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+const mostrar = document.querySelector("#mostrar")
+// async function getDataUser() {
+
+//     const res = await fetch("https://mocki.io/v1/f360b0a6-1a00-4614-b250-c70bec083fb0");
+//     const data = await  res.json();
+//     const { email, id } = data.results[0];
+//     mostrar.innerHTML = `Tu email es: ${email} y el id es ${id.name}`
+// }
+// getDataUser();
+
+// async function getTask() {
+//     try {
+//         const res = await fetch("https://mocki.io/v1/0b765f00-0010-4b79-8cea-451ced12b339");
+//         const data = await res.json();
+//         console.log(data);
+//         const { task }  = data.data;
+//         console.log(task[0].title);
+//     } catch (error) {
+//         console.log('ocurrió un error ', error);
+//     }
+// };
+// getTask();
+
+
+// async function getCity() {
+//     try {
+//         const resp = await fetch("https://openweathermap.org/=${apiKey}");
+//         const data = resp.json();
+//     } catch (error) {
+        
+//     }
+// }
