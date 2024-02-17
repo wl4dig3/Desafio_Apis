b2a873d49a23ac584f6c850aed7a613a223d4c03
wl4dig3 <wladige@gmail.com>
2024-02-16 21:57:32 -0300

    feat: se refactoriza con try y catch

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,44 +1,46 @@
-function converter() {
-  fetch("https://mindicador.cl/api/")
-    .then((resp) => resp.json())
-    .then((data) => {
-      const { dolar, euro, bitcoin, uf, utm, ivp } = data;
+async function  converter() {
+  
+  try {
+    const resp = await fetch("https://mindicador.cl/api/");
+    const data = await resp.json();
+    const { dolar, euro, bitcoin, uf, utm, ivp } = data;
 
-      const $ = document;
-      let moneda = $.getElementById("seleccion-moneda").value;
-      console.log("moneda", moneda);
-      let obtenerMonto = parseFloat($.getElementById("input-monto").value);
-      console.log("monto", obtenerMonto);
+    const $ = document;
+    let moneda = $.getElementById("seleccion-moneda").value;
+    console.log("moneda", moneda);
+    let obtenerMonto = parseFloat($.getElementById("input-monto").value);
+    console.log("monto", obtenerMonto);
 
-      $.getElementById("btnBuscar").addEventListener("click", converter);
-      let resultado;
+    $.getElementById("btnBuscar").addEventListener("click", converter);
+    let resultado;
 
-      switch (moneda) {
-        case "bitcoin":
-          resultado = obtenerMonto / bitcoin.valor;
-          break;
-        case "dolar":
-          resultado = obtenerMonto / dolar.valor;
-          break;
-        case "euros":
-          resultado = obtenerMonto / euro.valor;
-          break;
-        case "uf":
-          resultado = obtenerMonto / uf.valor;
-          break;
-        case "utm":
-          resultado = obtenerMonto / utm.valor;
-          break;
-        case "ivp":
-          resultado = obtenerMonto / ivp.valor;
-          break;
-        default:
-          resultado = "favor ingresa algo";
-          break;
-      }
-      $.getElementById("resultado").textContent = resultado.toFixed(4);
-      $.getElementById("resultado").style.color = "white"
-    })
-    .catch((error) => console.error("Error en la api", error));
+    switch (moneda) {
+      case "bitcoin":
+        resultado = obtenerMonto / bitcoin.valor;
+        break;
+      case "dolar":
+        resultado = obtenerMonto / dolar.valor;
+        break;
+      case "euros":
+        resultado = obtenerMonto / euro.valor;
+        break;
+      case "uf":
+        resultado = obtenerMonto / uf.valor;
+        break;
+      case "utm":
+        resultado = obtenerMonto / utm.valor;
+        break;
+      case "ivp":
+        resultado = obtenerMonto / ivp.valor;
+        break;
+      default:
+        resultado = "favor ingresa algo";
+        break;
+    }
+    $.getElementById("resultado").textContent = resultado.toFixed(4);
+    $.getElementById("resultado").style.color = "white"
+  }catch (error) {
+     console.error("Error en la api", error);
+  }
 }
 converter();
